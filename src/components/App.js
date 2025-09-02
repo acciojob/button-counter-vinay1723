d4f3460859956038d47e3172a7bcb38cf64733f0
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-09-02 22:02:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+ const [counter, setCounter] = useState(0);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {counter} times</p>
+      <button onClick={() => setCounter(num => num + 1)}>Click me</button>
     </div>
-  )
+  );
 }
 
 export default App
